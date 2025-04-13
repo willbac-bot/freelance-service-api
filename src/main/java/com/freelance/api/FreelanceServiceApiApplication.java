e9e7f0c81bf82ca170cf02a2d1617821aa241b6f
William Bacelar <william@vallus.com.br>
2025-04-13 16:42:08 -0300

    chore: first commit getninjas api application

---
+++ src/main/java/com/freelance/api/FreelanceServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.freelance.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelanceServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freelance/api/FreelanceServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelance.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
